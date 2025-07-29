37a1d6f149d9f34bde82f7b39760a4b89e4c2c01
CYBERG-03 <it24103439@my.sliit.lk>
2025-07-29 11:31:42 +0530

    Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,18 @@
+// IT24103439 - Lab1
+
+import java.util.Scanner;
+
+public class Welcome {
+  public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("Enter your First Name: ");
+	String fname = input.nextLine();
+	System.out.print("Enter your Last Name: ");
+	String lname = input.nextLine();
+
+	System.out.println("Welcome to the Second Year " + fname + " " + lname);
+
+	input.close();
+  }
+}
